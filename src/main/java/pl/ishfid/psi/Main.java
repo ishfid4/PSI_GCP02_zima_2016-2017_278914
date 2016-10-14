ed73a891d96c9b2774a36d218fae4d04d2fa78ba
ishfid4 <ishfid4@gmail.com>
2016-10-14 19:49:41 +0200

    Basic netwoerk using ECOG lib, XOR problem

--- src/main/java/pl/ishfid/psi/Main.java
+++ src/main/java/pl/ishfid/psi/Main.java
@@ -1,6 +1,15 @@
 package pl.ishfid.psi;
 
 import javafx.util.Pair;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.MLRegression;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
 import java.util.ArrayList;
 
@@ -9,7 +18,10 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args){
-        Neuron perceptron = new Neuron(0.5);
+
+        // ------ Single Neuron Perceptor ------
+        // OR problem
+/*        Neuron perceptron = new Neuron(0.5);
         double[][] inputs = {
                 {0, 0},
                 {0, 1},
@@ -21,9 +33,53 @@ public class Main {
 
         perceptron.learn(inputs, outputs, .01);
 
-        System.out.println(perceptron.output(new double[]{0, 0}));
-        System.out.println(perceptron.output(new double[]{0, 1}));
-        System.out.println(perceptron.output(new double[]{1, 0}));
-        System.out.println(perceptron.output(new double[]{1, 1}));
+        System.out.println("For Input: 0, 0 -> Output: "+ perceptron.output(new double[]{0, 0}));
+        System.out.println("For Input: 0, 1 -> Output: "+ perceptron.output(new double[]{0, 1}));
+        System.out.println("For Input: 1, 0 -> Output: "+ perceptron.output(new double[]{1, 0}));
+        System.out.println("For Input: 1, 1 -> Output: "+ perceptron.output(new double[]{1, 1}));
+*/
+
+        // -----------XOR Proplem -------------
+        double[][] xorInput = {
+                {0.0, 0.0},
+                {0.0, 1.0},
+                {1.0, 0.0},
+                {1.0, 1.0}
+        };
+
+        double[][] xorIdealOutput = {
+                {0.0},
+                {1.0},
+                {1.0},
+                {0.0}
+        };
+
+        // Creating training data set
+        BasicMLDataSet trainingSet = new BasicMLDataSet(xorInput, xorIdealOutput);
+
+        // Creating basic network
+        BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(null, true, 2)); // Input layer
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 2)); // Hiden layer
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 1)); // Output layer
+        network.getStructure().finalizeStructure(); // Ends setting/adding layers
+        network.reset(); // Initialize randomly connections weight
+
+        // Training
+        ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+
+        int epoch = 0; // epoch = iteration
+        do {
+            train.iteration();
+            epoch++;
+            System.out.println(" Epoch No: " + epoch + ", Error: " + train.getError());
+        }while (train.getError() > 0.001);
+
+        // Evaluation / Testing
+        for (MLDataPair item: trainingSet) {
+            MLData output = network.compute(item.getInput());
+            System.out.println("Input: " + item.getInput().getData(0) + ", " + item.getInput().getData(1) + " Ideal: "
+                    + item.getIdeal().getData(0) + "  Actual: " + output.getData(0));
+        }
     }
 }
